75c8066192c3c6c81022c5f3b7dc4a3e0532830b
rabab raza ali <robin.ali85@yahoo.com>
2019-07-11 19:01:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+addition (num1 + num2)
 end
 
 def subtraction(num1, num2)
-
+subtraction( num 2- num1)
 end
 
 def division(num1, num2)
-
+division (num1 / num2)
 end
 
 def multiplication(num1, num2)
-
+multiplication (num1 x num 2)
 end
 
 def modulo(num1, num2)
-
+modulo (num 1/num 2)
 end
 
 def square_root(num)
-
+square root num 
 end
